03d78d1beb7374d817c4fba004daf5cad1fea4eb
jung <jeongy95@naver.com>
2019-02-12 18:19:29 +0900

    시간으로 타이머 설정하는 기능 추가

--- parsers.py
+++ parsers.py
@@ -1,11 +1,21 @@
 import re
+import datetime as dt
 
 
 class ParseError(Exception):
     pass
 
 
-_re_time_str = re.compile(r'^(?P<size>[\d]+)(?P<unit>[smh])$')
+_re_time_str = re.compile(r'^(?P<size>[\d]+)(?P<is_time>:?)(?P<unit>[smh]|\d{1,2})$')
+
+def get_time_diff(t) -> int:
+    """
+    Get time difference from now
+    """
+    _current_time = dt.datetime.now().strftime('%H:%M')
+    current_time = dt.datetime.strptime(_current_time, '%H:%M')
+    input_time = dt.datetime.strptime(t, '%H:%M')
+    return (input_time - current_time).seconds
 
 def parse_time_str(s) -> int:
     """
@@ -16,12 +26,20 @@ def parse_time_str(s) -> int:
         raise ParseError 
 
     size = int(match.group('size'))
+    is_time = True if match.group('is_time') == ':' else False
     unit = match.group('unit')
+
     if unit == 's':
         return size
     elif unit == 'm':
         return size * 60
     elif unit == 'h':
         return size * 60 * 60
+    elif is_time and unit.isdigit() and 0 <= size < 24 and 0 <= int(unit) < 60:
+        hour = size
+        minute = int(unit)
+        seconds = get_time_diff(f'{hour}:{minute}')
+        seconds = seconds if seconds >= 0 else seconds + 60 * 60 * 24   # if input time is smaller then it is timer for next day
+        return seconds
     else:
         raise ParseError
