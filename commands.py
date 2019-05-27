0db18d1b7628490b86e1eab807d8307d3cde6efd
Do Hoerin <hellodhlyn@gmail.com>
2019-05-27 19:31:50 +0900

    택배조회 명령어 예외처리 추가

--- commands.py
+++ commands.py
@@ -93,33 +93,43 @@ def _track_delivery(carrier, track_id, bot, update, first_call=False):
     track = delivery.get_tracking(carrier['id'], track_id)
     msg = f"\n[배송 정보]\n운송장 번호 : {track_id}\n"
 
-    if 'progresses' in track:
+    follow_up = False
+    if 'progresses' in track and len(track['progresses']) == 0:
+        if first_call:
+            msg += '배송 준비중입니다.'
+            _reply_text(msg, bot, update)
+
+        follow_up = True
+    elif 'progresses' in track and len(track['progresses']) > 0:
         current = sorted(track['progresses'], key=lambda p: p['time'])[-1]
         if current['status']['id'] == 'delivered':
             msg += '배송이 완료되었습니다.'
             _reply_text(msg, bot, update)
         else:
             dt = datetime.strptime(current['time'], '%Y-%m-%dT%H:%M:%S%z')
-            if first_call or dt >= (datetime.now(timezone.utc) - timedelta(hours=1)):
+            if first_call or dt >= (datetime.now(timezone.utc) - timedelta(minutes=30)):
                 msg += f"상태 : {current['status']['text']}\n" + \
                     f"위치 : {current['location']['name']}\n\n" + \
                     f"{current['description']}\n" + \
                     f"(업데이트 : {(datetime.now(timezone.utc) - dt).seconds // 60}분 전)"
                 _reply_text(msg, bot, update)
-
-            def schedule():
-                import sched
-                import time
-
-                scheduler = sched.scheduler(time.time, time.sleep)
-                scheduler.enter(30 * 60, 1, _track_delivery, argument=(carrier, track_id, bot, update))
-                scheduler.run()
             
-            thread = threading.Thread(target=schedule)
-            thread.start()
-
-            if first_call:
-                _reply_text('배송 내역에 변경이 있을 시 30분 간격으로 알림이 발송됩니다.', bot, update)
+            follow_up = True
     else:
         msg += "올바르지 않은 운송장이거나, 택배사에서 아직 물건을 인수하지 않았습니다."
         _reply_text(msg, bot, update)
+
+    if follow_up:
+        def schedule():
+            import sched
+            import time
+
+            scheduler = sched.scheduler(time.time, time.sleep)
+            scheduler.enter(30 * 60, 1, _track_delivery, argument=(carrier, track_id, bot, update))
+            scheduler.run()
+        
+        thread = threading.Thread(target=schedule)
+        thread.start()
+
+        if first_call:
+            _reply_text('배송 내역에 변경이 있을 시 30분 간격으로 알림이 발송됩니다.', bot, update)
